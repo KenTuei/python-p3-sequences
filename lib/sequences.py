78d2c2aee1fd02c26e06e385e8112e2e5b04b11e
ken <kentuei05@gmail.com>
2025-05-07 17:23:23 +0300

    sequence

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
-#!/usr/bin/env python3
+# #!/usr/bin/env python3
 
+# def print_fibonacci(length):
+#     sequence = []
+#     a, b =0, 1
+#     for _in range(length):
+#         sequence.append(a)
+#     a, b = b, a + b
+# print(sequence)
 def print_fibonacci(length):
-    pass
+    sequence = []
+    a, b = 0, 1
+    for _ in range(length):
+        sequence.append(a)
+        a, b = b, a + b
+    print(sequence)
